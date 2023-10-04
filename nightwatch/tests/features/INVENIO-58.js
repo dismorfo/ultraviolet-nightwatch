c3a98622fe4e4fa63d9a8c60da7edde0129795dd
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-10-04 12:11:11 -0700

    Add test for * required

--- nightwatch/tests/features/INVENIO-58.js
+++ nightwatch/tests/features/INVENIO-58.js
@@ -15,7 +15,8 @@ describe('INVENIO-58', function() {
     // browser.saveScreenshot('./images/community.png')
   })
 
-  it("Has an asterisk next to the Communities Selection Text", function() {
+  it("Has an asterisk next to the Selected Community text to indicate it's required", function() {
+    const parentSelector = "#deposit-form div.page-subheader-element:nth-last-child(2)";
 
     browser.execute(function (parentSelector) {
       const parentElement = document.querySelector(parentSelector);
@@ -23,13 +24,11 @@ describe('INVENIO-58', function() {
       return computedStyle.content;
     }, [parentSelector], function (result) {
       const afterContent = result.value
-
-      // Perform an assertion on the ::after content
-      browser.assert.equal(afterContent, '"*"', '::after content is correct (it is "*")')
+      
+      // Perform an assertion on the :after content
+      browser.assert.equal(afterContent, "\"*\"", ':after content is correct (it is "*")')
     })
-
   })
 
   after(browser => browser.end())
-
 })
