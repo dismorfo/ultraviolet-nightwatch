39072e5b447b94e959e66cc5f28fc0fbf47298fc
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-10-04 12:10:25 -0700

    Add selector to upload page

--- nightwatch/pages/upload.js
+++ nightwatch/pages/upload.js
@@ -1,7 +1,9 @@
 module.exports = {
     commands: [],
     elements: {
-
+        'communityTitle':  {
+            selector: "#deposit-form div.page-subheader-element:nth-last-child(2)"
+        },
     },
     url: function() {
         return this.api.launch_url + '/uploads/new'
