e31c1007bff4acad246128f040fd020e3af8f3be
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-10-10 15:56:34 -0700

    Add selector for warning message

--- nightwatch/pages/upload.js
+++ nightwatch/pages/upload.js
@@ -2,7 +2,10 @@ module.exports = {
     commands: [],
     elements: {
         'communityTitle':  {
-            selector: "#deposit-form div.page-subheader-element:nth-last-child(2)"
+          selector: "#deposit-form div.page-subheader-element:nth-last-child(2)"
+        },
+        'fileUploadAlertMessages': {
+          selector: "div.ui.visible.warning.message"
         },
     },
     url: function() {
