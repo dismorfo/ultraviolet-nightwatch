92e5cd7a9902edfd16b5641036ae40f942718f45
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-02 16:56:26 -0700

    Add check for 100 files text

--- nightwatch/tests/features/INVENIO-84.js
+++ nightwatch/tests/features/INVENIO-84.js
@@ -16,7 +16,14 @@ describe('INVENIO-84', function() {
   it('Has a second alert message in the file upload section warning about file size', function(browser) {
     const searchText = "Contact the NYU Libraries UltraViolet service team at uv@nyu.edu if you have more than 100 files or 50GB."
 
-    browser.page.upload().element.findAll('@fileUploadAlertMessages').nth(1).findByText(searchText).assert.visible();
+    browser.page.upload().element.findAll('@fileUploadAlertMessages').nth(1).assert.visible();
+    browser.page.upload().element.findAll('@fileUploadAlertMessages').nth(1).find("p").assert.visible();
+    browser.element.findAll('div.ui.visible.warning.message').nth(1).assert.hasDescendants();
+    browser.element.findAll('div.ui.visible.warning.message').nth(1).find('p').assert.textContains("100 files");
+    // browser.assert.textContains("div.ui.visible.warning.message::nth(1) p", "100 files");
+    // browser.page.upload().element.findAll('@fileUploadAlertMessages').nth(1).assert.containsText('.mainline-results', 'Nightwatch.js')
+    // browser.page.upload().element.findByText(searchText).assert.visible();
+    // browser.page.upload().element.findByText('100 files').assert.visible();
   })
 
   after(browser => browser.end())
